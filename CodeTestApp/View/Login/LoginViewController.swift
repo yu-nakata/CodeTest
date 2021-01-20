41a21de75a78db1abc18f21c31021ec27c5e51fd
Yuki Nakata <yu.nakata.work@gmail.com>
2021-01-20 19:44:17 +0900

    [add] ログイン画面とメイン画面のファイルだけ追加

---
+++ CodeTestApp/View/Login/LoginViewController.swift
@@ -0,0 +1,12 @@
+//
+//  LoginViewController.swift
+//  CodeTestApp
+//
+//  Created by 中田祐稀 on 2021/01/20.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+}
---
+++ CodeTestApp/View/Main/MainViewController.swift
@@ -0,0 +1,8 @@
+//
+//  MainViewController.swift
+//  CodeTestApp
+//
+//  Created by 中田祐稀 on 2021/01/20.
+//
+
+import Foundation
